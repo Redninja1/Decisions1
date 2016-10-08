6534f4997ca9bc23d5f8a9364447b32210726d60
Redninja1 <james@redninja.net>
2016-10-08 19:34:47 +0100

    Decisions 1

---
+++ Decisions1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decisions1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please type something and press Enter key.");
+            string userValue;
+            userValue = Console.ReadLine();
+            Console.WriteLine("You typed: " + userValue);
+            Console.ReadLine();
+
+        }
+    }
+}
